ad883f00e72531fdf66369bf4a036782a2387503
4forces <toh.tingji@gmail.com>
2020-11-08 08:28:31 +0000

    Step 12.7 done - 'Create Route for Add books'

--- books/views.py
+++ books/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect, reverse
 from .models import Book, Author2
 from .forms import BookForm
 # Create your views here.
+
+
 def index(request):
     books = Book.objects.all()
     # passing data as dict to render() function
     return render(request, 'books/index.template.html', {
-        'books':books
+        'books': books
     })
 
 
@@ -14,12 +16,28 @@ def authors(request):
     authors = Author2.objects.all()
     # passing data as dict to render() function
     return render(request, 'books/authors.template.html', {
-        'authors':authors
+        'authors': authors
     })
 
 
 def create_book(request):
-    create_book_form = BookForm()
-    return render(request, 'books/create_book.template.html', {
-        'form':create_book_form
-    })
+    if request.method == "POST":
+        # # checks if 'POST' is working
+        # return HttpResponse("form submitted")
+
+        # eqv. request.POST is the same request.form in Flask
+        # creates the form again, but pass in all user's input
+        # that has been submitted
+        form = BookForm(request.POST)
+        # is_valid() is a django function
+        if form.is_valid():
+            # save keyed in data to the database
+            form.save()
+            # eqv. to 'redirect(url_for(index))' in Flask
+            return redirect(reverse(index))
+    else:
+        # create an instance of the BookForm
+        create_book_form = BookForm()
+        return render(request, 'books/create_book.template.html', {
+            'form': create_book_form
+        })
