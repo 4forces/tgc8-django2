70f8ae5886061311e10c73a300f043845fbaeb7e
4forces <toh.tingji@gmail.com>
2020-11-20 01:49:44 +0000

    Import and register 'Genre' model in admin.py

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Book, Author, Author2
+from .models import Book, Author, Author2, Genre
 
 # Register your models here.
 admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Author2)
+admin.site.register(Genre)
