e797aa01a12e3951e3a26af7bfeaf6ebd9fb99e9
4forces <toh.tingji@gmail.com>
2020-11-23 13:15:12 +0000

    Step 39 | Update cart.views.py

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from books.model import Book
 
 # Create your views here.
+
+
+def add_to_cart(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+
+    # initialise the shopping cart if it does not exist
+    # or fetch the cart if it does
+    cart = request.session.get('shopping_cart', {})
+
+    if book_id not in cart:
+        book = get_object_or_404(Book, pk=book_id)
+        cart[book_id] = {
+            'id': book_id,
+            'title': book.title,
+            'cost': 99,
+            'qty': 1
+        }
+    else:
+        # if the shopping cart already has the book,
+        # then we assume the user want to buy an additional copy
+        cart[book_id]['qty'] += 1
+
+    request.session['shopping_cart'] = cart
+    return redirect(reverse('books.views.index'))
